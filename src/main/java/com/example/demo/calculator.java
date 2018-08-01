afb3d8ea122816dc237f3970aa6643932f7ca0d0
Marek Dąbkowski <marquess94@gmail.com>
2018-08-01 21:27:10 +0200

    First Commit

---
+++ src/main/java/com/example/demo/calculator.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class calculator {
+
+    public void SomeMethod() {
+            System.out.println("It works");
+        }
+
+    public static void main(String args[]) {
+        calculator calculator = new calculator();
+        calculator.SomeMethod();
+    }
+}
